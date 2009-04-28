1482a2149654d89a1fe0e25e626a46862ad9f5fa
stefan@ott.net <stefan@ott.net@c343eafa-df13-11dd-9759-139c9ab14fc6>
2009-04-28 03:07:21 +0000

    Added some UI

--- trunk/planster/main/views.py
+++ trunk/planster/main/views.py
@@ -2,4 +2,22 @@
 # Copyright (C) 2009 Stefan Ott, all rights reserved
 #
 
-# Create your views here.
+from django.template import Context, loader
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from planster.main.models import Plan
+from django.template import RequestContext
+
+def index(request):
+	return render_to_response('welcome.html', {},
+		context_instance = RequestContext(request))
+
+def create(request):
+	plan = Plan()
+	return render_to_response('create.html', {'plan': plan},
+		context_instance = RequestContext(request))
+
+def plan(request, plan_id):
+	plan = Plan.objects.get(hash=plan_id)
+	return render_to_response('plan.html', {'plan': plan},
+		context_instance = RequestContext(request))
--- trunk/planster/settings.py
+++ trunk/planster/settings.py
@@ -35,17 +35,17 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/stefan/projects/planster/trunk/planster/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = 'http://localhost/~stefan/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/media2/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '5a()5^e7^k_-a8e!wkjo_dqii@0y8y-cirsf2d%3#)lfb3ik0$'
@@ -69,6 +69,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+	'/home/stefan/projects/planster/trunk/planster/templates'
 )
 
 INSTALLED_APPS = (
